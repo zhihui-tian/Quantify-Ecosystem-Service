6a1fdf746d79d340a8393f89ad59191a2991abda
zhihui-tian <89556786+zhihui-tian@users.noreply.github.com>
2023-09-22 19:41:03 -0400

    Add files via upload

---
+++ raster_truth_data.py
@@ -0,0 +1,54 @@
+"""
+For:
+Author: zhihu
+Date: 2023/01/16
+"""
+# from osgeo import gdal,ogr
+# #import ogr
+#
+#
+# naip_fn="C:/Users/zhihui.tian/Desktop/merged_image.tif"
+# naip_ds=gdal.Open(naip_fn)
+#
+#
+#
+# train_fn='C:/Users/zhihui.tian/Desktop/1/naip/m_2908222_sw_17_060_20211129/train.shp'
+# train_ds=ogr.Open(train_fn)
+# lyr=train_ds.GetLayer()
+# driver=gdal.GetDriverByName('MEM')
+# target_ds=driver.Create('',naip_ds.RasterXSize, naip_ds.RasterYSize,1,gdal.GDT_UInt16)
+# target_ds.SetGeoTransform(naip_ds.GetGeoTransform())
+# target_ds.SetProjection(naip_ds.GetProjection())
+# options=['ATTRIBUTE=id']
+# gdal.RasterizeLayer(target_ds,[1],lyr,options=options)
+# data=target_ds.GetRasterBand(1).ReadAsArray()
+# print('min',data.min(),'max',data.max(),'mean',data.mean())
+
+"""
+For:
+Author: zhihu
+Date: 2023/01/16
+"""
+from osgeo import gdal,ogr
+#import ogr
+
+
+naip_fn="C:/Users/zhihui.tian/Downloads/3/3.tif"
+naip_ds=gdal.Open(naip_fn)
+
+
+
+train_fn='C:/Users/zhihui.tian/Desktop/1/naip/m_2908222_sw_17_060_20211129/train.shp'
+train_ds=ogr.Open(train_fn)
+lyr=train_ds.GetLayer()
+driver=gdal.GetDriverByName('MEM')
+target_ds=driver.Create('',naip_ds.RasterXSize, naip_ds.RasterYSize,1,gdal.GDT_UInt16)
+target_ds.SetGeoTransform(naip_ds.GetGeoTransform())
+target_ds.SetProjection(naip_ds.GetProjection())
+options=['ATTRIBUTE=id']
+gdal.RasterizeLayer(target_ds,[1],lyr,options=options)
+data=target_ds.GetRasterBand(1).ReadAsArray()
+print('min',data.min(),'max',data.max(),'mean',data.mean())
+
+
+
