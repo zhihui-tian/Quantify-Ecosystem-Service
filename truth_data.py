3700d0023e7648027512bb792e213ca8061e0712
zhihui-tian <89556786+zhihui-tian@users.noreply.github.com>
2023-09-22 20:05:45 -0400

    Update truth_data.py

--- truth_data.py
+++ truth_data.py
@@ -4,40 +4,6 @@ Author: zhihu
 Date: 2023/01/12
 """
 
-# import numpy as np
-# import geopandas as gpd
-# import pandas as pd
-#
-# import fiona
-#
-# shape = fiona.open("C:/Users/zhihui.tian/Desktop/4.16/ground_truth.shp")
-# print(shape.schema)
-#
-# gdf=gpd.read_file("C:/Users/zhihui.tian/Desktop/4.16/ground_truth.shp")
-# gdf= gdf.loc[(gdf['SAMPLE_1'] != '`7') & (gdf['SAMPLE_1'] != "9'")]
-# class_names=gdf['SAMPLE_1'].unique()
-# print('class names',class_names)
-# class_ids=np.arange(class_names.size)+1
-# print('class ids',class_ids)
-# df=pd.DataFrame({'labels':class_names,'id':class_ids})
-# df.to_csv("C:/Users/zhihui.tian/Desktop/1/naip/m_2908222_sw_17_060_20211129/class_lookup.csv")
-# print("gdf without ids",gdf.head())
-# gdf['id']=gdf['SAMPLE_1'].map(dict(zip(class_names,class_ids)))
-# print('gdf with ids',gdf.head())
-#
-# gdf_train=gdf.sample(frac=0.7)
-# gdf_test=gdf.drop(gdf_train.index)
-# print('gdf shape',gdf.shape,'training shape',gdf_train.shape,'test',gdf_test.shape)
-# gdf_train.to_file("C:/Users/zhihui.tian/Desktop/1/naip/m_2908222_sw_17_060_20211129/train.shp")
-# gdf_test.to_file("C:/Users/zhihui.tian/Desktop/1/naip/m_2908222_sw_17_060_20211129/test.shp")
-
-
-"""
-For:
-Author: zhihu
-Date: 2023/01/12
-"""
-
 import numpy as np
 import geopandas as gpd
 import pandas as pd
